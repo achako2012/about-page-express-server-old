6865ae0c6684cfdbf91758e3b43cef9f80b32804
alex <a.chako2012@gmail.com>
2021-12-16 13:02:37 +0100

    WIP

--- shared/controllers/auth-api-controller.d.ts
+++ shared/controllers/auth-api-controller.d.ts
@@ -1,4 +1,13 @@
 import { Request, Response } from 'express';
-export declare const register: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const login: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const deleteUser: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
+export declare const register: (
+    req: Request,
+    res: Response
+) => Promise<Response<any, Record<string, any>>>;
+export declare const login: (
+    req: Request,
+    res: Response
+) => Promise<Response<any, Record<string, any>>>;
+export declare const deleteUser: (
+    req: Request,
+    res: Response
+) => Promise<Response<any, Record<string, any>>>;
