2a78b14a64d54f7e68dd4ba44fdbc3a773c4ce50
achako2012 <a.chako2012@gmail.com>
2022-02-06 22:13:24 +0100

    WIP

--- shared/server.js
+++ shared/server.js
@@ -25,6 +25,7 @@ server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 server.use(express.json({ limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
+server.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 if (process.env.NODE_ENV === 'production') {
     server.use(express.static('client/build'));
 }
