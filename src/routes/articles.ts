468b97e943be1a0e6226d6c074d54da276e0d7ee
Oleksandr Chako <a.chako2012@gmail.com>
2022-06-02 21:39:07 +0300

    WIP

--- src/routes/articles.ts
+++ src/routes/articles.ts
@@ -11,7 +11,7 @@ const router = Router();
 
 router.get('/articles-api/articles-list', getArticles);
 
-router.post('/articles-api/articles-listss', createArticle);
+router.post('/articles-api/articles-listfoo', createArticle);
 
 router.put('/articles-api/articles-list', updateArticleById);
 
