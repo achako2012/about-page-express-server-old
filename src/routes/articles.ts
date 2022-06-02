fad506cddf748e8a91c52123b0bab884ba6b1da3
Oleksandr Chako <a.chako2012@gmail.com>
2022-06-02 23:10:44 +0300

    WIP

--- src/routes/articles.ts
+++ src/routes/articles.ts
@@ -12,9 +12,9 @@ const router = Router();
 
 router.get('/articles-api/articles-list', getArticles);
 
-router.post('/articles-api/articles-list', bodyParser({limit: '50mb'}),createArticle);
+router.post('/articles-api/articles-list', createArticle);
 
-router.put('/articles-api/articles-list', bodyParser({limit: '50mb'}), updateArticleById);
+router.put('/articles-api/articles-list', updateArticleById);
 
 router.delete('/articles-api/articles-list', deleteArticleById);
 
